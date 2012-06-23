d504a54a0ccf6e15ae57b9d98c3e3d35a256a8b2
Adrian Peterson Co <adrianpco@gmail.com>
2012-06-23 12:40:35 +0800

    Adds new layout, needs more fix for the layout

--- src/com/badsectorlabs/remindme/TaskDetailsActivity.java
+++ src/com/badsectorlabs/remindme/TaskDetailsActivity.java
@@ -3,19 +3,26 @@ package com.badsectorlabs.remindme;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Debug;
+import android.view.View;
 import android.widget.TextView;
 
 public class TaskDetailsActivity extends Activity{
 	@Override
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
-		
+		setContentView(R.layout.details);
+		TextView titleName = (TextView) findViewById(R.id.titlename);
 		Intent intent = getIntent();
 		String title = intent.getStringExtra(Main.TITLE);
+		titleName.setText(title);
+	}
+	
+	public void onDestroy(){
+		super.onDestroy();
+		Debug.stopMethodTracing();
+	}
+	public void addDetails(View view){
 		
-		TextView textView = new TextView(this);
-		textView.setTextSize(40);
-		textView.setText(title);
-		setContentView(textView);
 	}
 }
